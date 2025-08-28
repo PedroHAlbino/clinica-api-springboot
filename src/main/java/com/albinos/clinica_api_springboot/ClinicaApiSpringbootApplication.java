d2838d48b693c65980dd027591faecaa0108a780
money-api <henriquealbino1@gmail.com>
2025-08-28 00:12:37 -0300

    Projeto Zerado

---
+++ src/main/java/com/albinos/clinica_api_springboot/ClinicaApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.albinos.clinica_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albinos/clinica_api_springboot/ClinicaApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albinos.clinica_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
